1b71c93eb96c6f611201f4fc2849fab88e97b779
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2023-07-31 14:34:13 +0200

    Add url and classifiers

--- setup.py
+++ setup.py
@@ -4,9 +4,17 @@ setup(
     name='eofetch',
     version='0.1',
     description='Earth Observation Data Retrieval library',
+    url="https://github.com/stcorp/eofetch",
     author='S[&]T',
     license='BSD',
     packages=['eofetch'],
     entry_points={'console_scripts': ['eofetch = eofetch.main:main']},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+    ],
     install_requires=['boto3', 'pystac-client', 'requests']
 )
