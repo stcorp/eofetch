c4be6c36285e979c3054a73bbe0717eb1217be83
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2023-08-02 12:49:58 +0200

    Upgrade to version 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='eofetch',
-    version='0.1',
+    version='0.2',
     description='Earth Observation Data Retrieval library',
     url="https://github.com/stcorp/eofetch",
     author='S[&]T',
