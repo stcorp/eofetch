318e0f1c797209426a481b38e30ac71b5a06c2e7
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2025-03-28 16:09:44 +0100

    Use 1MB download chunks

--- eofetch/s5ppal.py
+++ eofetch/s5ppal.py
@@ -17,5 +17,5 @@ def download(product, target_directory):
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
         with open(os.path.join(target_directory, product), 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
+            for chunk in r.iter_content(1048576):  # use 1MB blocks
                 f.write(chunk)
