d032afaef3fb4527957dcfb446a2b1ed5ce85c4e
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2025-09-30 15:21:09 +0200

    Bump version to 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='eofetch',
-    version='0.3',
+    version='0.4',
     description='Earth Observation Data Retrieval library',
     url="https://github.com/stcorp/eofetch",
     author='S[&]T',
